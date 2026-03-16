75d2a634e692b8bc0a2b04b4aff29a958764b6ec
shibin <shibin>
2026-03-16 14:17:57 +0530

    cors error fix

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: ["http://localhost:5174",'nexeramarket.vercel.app'],
+    origin: ["http://localhost:5174",'nexeramarket.vercel.app','https://nexera-automotive-server-freelanc.onrender.com'],
     credentials: true
   })
 );
