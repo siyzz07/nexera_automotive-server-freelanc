9d0f64ba3c1b76ebfafb66c3580c89b77b270dbb
shibin <shibin>
2026-03-16 15:41:50 +0530

    fix: correct CORS allowed git push '

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: ["http://localhost:5174",'nexeramarket.vercel.app','https://nexera-automotive-server-freelanc.onrender.com'],
+    origin: ["http://localhost:5174", "https://nexeramarket.vercel.app"],
     credentials: true
   })
 );
